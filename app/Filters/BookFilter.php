f8fb77ecd14c56e5dbae855e770feaf6fa9a7280
Joseph Akitoye <josephakitoye@gmail.com>
2020-05-09 13:48:33 +0000

    Add filter and cleanup duplications in sidebar
    Signed-off-by: Joseph Akitoye <josephakitoye@gmail.com>

--- app/Filters/BookFilter.php
+++ app/Filters/BookFilter.php
@@ -3,34 +3,39 @@
 namespace App\Filters;
 
 
-use App\Models\Book;
 use Illuminate\Database\Eloquent\Builder;
 
 class BookFilter extends QueryFilter
 {
+    /**
+     * Eloquent builder instance.
+     *
+     * @var
+     */
+    protected $builder;
+
     public function year( $year )
     {
-        return Book::where( 'year', $year );
+        return $this->builder->where( 'year', $year );
     }
 
     public function genre( $genre )
     {
-        return Book::whereHas( 'genres', function ( Builder $builder ) use ( $genre ) {
+        return $this->builder->whereHas( 'genres', function ( Builder $builder ) use ( $genre ) {
             $builder->whereName( $genre )->distinct();
         } );
     }
 
     public function author( $author )
     {
-        return Book::whereHas( 'authors', function ( Builder $builder ) use ( $author ) {
+        return $this->builder->whereHas( 'authors', function ( Builder $builder ) use ( $author ) {
             $builder->whereName( $author )->distinct();
         } );
     }
 
     public function query( $query )
     {
-        return Book::where( 'title', 'like', "%{$query}%" )
-            ->orWhere( 'isbn', 'like', "%{$query}%" )
-            ;
+        return $this->builder->where( 'title', 'like', "%{$query}%" )
+            ->orWhere( 'isbn', 'like', "%{$query}%" );
     }
 }
--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -6,8 +6,6 @@ use App\Filters\BookFilter;
 use App\Models\Book;
 use Illuminate\Contracts\Foundation\Application;
 use Illuminate\Contracts\View\Factory;
-use Illuminate\Database\Eloquent\Builder;
-use Illuminate\Http\Request;
 use Illuminate\View\View;
 
 class BookController extends Controller
@@ -15,42 +13,17 @@ class BookController extends Controller
     /**
      * View all books.
      *
-     * @param Request $request
+     * @param BookFilter $request
      * @return Application|Factory|View
      */
-    public function index( Request $request )
+    public function index( BookFilter $request )
     {
-        $books   = $this->getBooks( $request );
-        $authors = $books->pluck( 'authors' )->flatten();
-        $genres  = $books->pluck( 'genres' )->flatten();
+        $books   = Book::filter( $request )->paginate();
+        $authors = $books->pluck( 'authors' )->flatten()->unique();
+        $genres  = $books->pluck( 'genres' )->flatten()->unique();
         $years   = $books->pluck( 'year' )->sortDesc();
 
         return view( 'books.index', compact( 'books', 'authors', 'genres', 'years' ) );
-    }
-
-    private function getBooks( Request $request )
-    {
-        if ( $request->has( 'author' ) || $request->has( 'genre' ) || $request->has( 'query' ) || $request->has( 'year' ) ) {
-            if ( $request->has( 'author' ) ) {
-                return Book::whereHas( 'authors', function ( Builder $builder ) use ( $request ) {
-                    $builder->whereName( $request->author );
-                } )->paginate();
-            }
-            if ( $request->has( 'query' ) ) {
-                return Book::where( 'title', 'like', "%{$request->get('query')}%" )
-                    ->orWhere( 'isbn', 'like', "%{$request->get('query')}%" )
-                    ->paginate();
-            }
-            if ( $request->has( 'genre' ) ) {
-                return Book::whereHas( 'genres', function ( Builder $builder ) use ( $request ) {
-                    $builder->whereName( $request->genre );
-                } )->paginate();
-            }
-            if ( $request->has( 'year' ) ) {
-                return Book::where( 'year', $request->year )->paginate();
-            }
-        }
-        return Book::paginate();
     }
 
     /**
